e2690429ccc61f5c3cacfe89d384f41e81c185f7
hohko <hohko@outlook.com>
2025-01-07 19:28:44 +0300

    1

--- manger.js
+++ manger.js
@@ -3,6 +3,7 @@ module.exports = {
      * @param {Creep} creeps
      */
     run: function(creeps){
+        //console.log("RUN")
         this.role = creeps.memory.role;
         if ((this.steck = creeps.memory.steck) === undefined){
             this.steck = [this.role];
