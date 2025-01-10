1eb580e3cf5f13f279c16bfe97be3c644fc8b457
hohko <hohko@outlook.com>
2025-01-11 00:32:18 +0300

    2

--- spawnManeger.js
+++ spawnManeger.js
@@ -70,7 +70,7 @@ function mem (spawn) {
         this.gMem = Memory.spawns.globalEvent
         this.count = 0;
     } else {
-        flagsconsole ? console.log('steck != undefined else'):0;
+        flagsconsole ? console.log('steck != undefined else'+steck):0;
         spawn.memory.steck = [[]];
         const gEvent = Memory.spawns.globalEvent;
         if(gEvent === undefined){
