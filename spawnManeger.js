ee1c8556c4e767cd2484541541e746d2c4943772
hohko <hohko@outlook.com>
2025-01-10 23:59:07 +0300

    2

--- spawnManeger.js
+++ spawnManeger.js
@@ -37,7 +37,7 @@ module.exports = {
         if(steck === undefined){
             return room;
         }
-        //console.log(steck);
+        console.log(steck);
         let error = FSM.action[steck[0]](spawn,steck[1],steck[3]);
         if(error === 0){
             this.mem.shiftLocal(spawn.name);
