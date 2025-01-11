c1cdc29589c19cf7ec278472cb5e77500c656e8b
hohko <hohko@outlook.com>
2025-01-11 22:21:12 +0300

    2

--- structure.spawn.js
+++ structure.spawn.js
@@ -8,13 +8,11 @@ module.exports = {
     /**@param {StructureSpawn} spawn */
     start:function(spawn){
         flagsconsole?console.log("structure.spawns=>start"):0;
-
-        const roomName = {spawnName:spawn.name, roomName:spawn.room.name};
         const MemSave = Memory.spawns[spawn.name];
         if( MemSave === undefined){
             flagsconsole?console.log("------Memory.spawn = {"+spawn.name+"}"):0;
             Memory.spawns = {[spawn.name]:save}; 
-            return undefined;       
+            return ;       
         } else {
             if(memorySpawn[spawn.name] === undefined){
                 flagsconsole?console.log("------chek memorySpawn"):0;
@@ -26,7 +24,7 @@ module.exports = {
 
 
         memorySpawn.saveMemory(spawn);
-        return roomName;
+        return ;
     }
 
 
