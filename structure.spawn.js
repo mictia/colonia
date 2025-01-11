a82b3b9b3efdda41321727d78017fccc71edbf04
hohko <hohko@outlook.com>
2025-01-11 14:00:09 +0300

    2

--- structure.spawn.js
+++ structure.spawn.js
@@ -1,4 +1,4 @@
-
+const flagsconsole = true;
 
 
 
@@ -6,6 +6,7 @@
 
 module.exports = {
     start:function(spawn){
+        flagsconsole?console.log("structure.spawns=>start"):0;
         const roomName = {[spawn.name]:spawn.room.name};
         if(spawn.memory === undefined){
             console.log('Error')
