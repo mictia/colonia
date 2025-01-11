0ba77a3124f5dcc0edba4adcef8e5525379e3f6e
hohko <hohko@outlook.com>
2025-01-11 22:20:23 +0300

    2

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ const creep = require('creeps');
 const spawner = require('structure.spawn');
 
 const flagsconsole = true;
-let start = undefined;
+let start = false;
 module.exports.loop = function(){
     flagsconsole?console.log("loop"):0;
 
-    if(start === undefined){
-        start = meneger.init();
+    if(start === false){
+        meneger.init();
+        start = true;
     }
     for(let name in Game.spawns){
         spawner.start(Game.spawns[name]);
