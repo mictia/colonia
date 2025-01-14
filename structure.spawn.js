4ecd60dde1d059b3c5cdb64a0938c19b578a8725
hohko <hohko@outlook.com>
2025-01-14 22:27:45 +0300

    2

--- structure.spawn.js
+++ structure.spawn.js
@@ -12,6 +12,7 @@ module.exports = {
         flagsconsole?console.log("structure.spawns=>start"):0;
         let mem = spawn.memory;
         if(!mem){
+            console.log('structure.spawns=>start:mem');
             mem = spawn.memory = {steck:[],getId:'',role:'',energiType:0};
         }
         switch(mem.steck){
